d6a0bbce23c1577e5b012b8511ef2b549ee8ef2f
OWoolland <oliver.woolland@manchester.ac.uk>
2022-02-05 16:26:11 +0000

    Read example database into pandas dataframe, push to streamlit table

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
+import helpers as hp
+
 st.header("Hello")
+
+recipies = hp.getRecipies()
+st.table(recipies)
--- helpers.py
+++ helpers.py
@@ -1,1 +1,22 @@
 import streamlit as st
+import sqlite3 as sql
+import pandas as pd
+
+def getRecipies():
+    connection = sql.connect('database.sqlite')
+    cursor = connection.cursor()
+
+    displayFields = "recipe.id,recipe.name," \
+        "brewnote.brewDate,brewnote.og,brewnote.abv"
+    query = "SELECT " + displayFields + " from recipe" \
+        " INNER JOIN brewnote ON recipe.id=brewnote.recipe_id " \
+        " WHERE" \
+        " NOT recipe.folder='brewtarget'" \
+        " AND recipe.deleted='0'" \
+        " AND brewnote.deleted='0' " \
+        " ORDER BY recipe.name"
+
+    recipies = pd.read_sql_query(query, connection)
+
+    connection.close()
+    return recipies
