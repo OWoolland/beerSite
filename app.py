2450810c9c5a67bd495d1820d175a470c8847829
OWoolland <oliver.woolland@manchester.ac.uk>
2022-02-06 16:43:07 +0000

    Move to rendering html table

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ st.set_page_config(layout="wide")
 st.header("Hello")
 
 recipies = hp.getRecipies()
-st.dataframe(recipies)
-st.table(recipies)
+#st.dataframe(recipies)
+#st.table(recipies)
+st.markdown(recipies.to_html(), unsafe_allow_html=True)
+
+
