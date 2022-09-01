d85aedec63e517dbd15109dc4851bf456a0bd9f0
OWoolland <oliver_woolland@hotmail.co.uk>
2022-09-01 10:51:07 +0100

    Add css styling

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import bs4
 
 import helpers as hp
 
@@ -7,7 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    recipies = hp.getRecipies()
-   return recipies
+
+   soup = bs4.BeautifulSoup(recipies)
+   css_link = soup.new_tag("link",
+                           rel="stylesheet",
+                           href="static/main.css")
+
+   soup.head.append(css_link)
+   
+   return str(soup)
 
 if __name__ == "__main__":
    app.run()
